416539e8088ba94b64e0b3daf6796707e72783e8
Bruno Papini <papinibh@hotmail.com>
2019-08-01 21:43:06 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -3,15 +3,36 @@
 	require_once("config.php");
 
 	/*$sql = new Sql();
-
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+	echo json_encode($usuarios);
+	*/
 
-	echo json_encode($usuarios);*/
 
+	//carrega um usuário
+	/*
 	$root = new Usuario();
-
 	$root->loadById(3);
-
 	echo $root;
+	*/
+
+
+	//carrega uma lista de usuários
+	/*
+	$lista = Usuario::getList();
+	echo json_encode($lista, JSON_UNESCAPED_SLASHES);
+	*/
+
+
+	//carrega uma lista de usuários buscando pelo login
+	/*
+	$search = Usuario::search("o");
+	echo json_encode($search);
+	*/
+
+
+	//carrega um usuário com base em seu login e senha
+	$usuario = new Usuario();
+	$usuario->login("Bruno", "0987654321");
+	echo $usuario;
 
 ?>
